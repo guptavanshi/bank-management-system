05d0f0ee291b342eec5008cbf5a09d29b73c44b9
Vanshika Gupta <165665558+guptavanshi@users.noreply.github.com>
2026-04-07 17:18:21 +0530

    Update database connection password in Conn.java

--- src/bank/management/system/Conn.java
+++ src/bank/management/system/Conn.java
@@ -12,7 +12,7 @@ public class Conn {
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem", "root", "vanshika123");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem", "root", "password");
             s = c.createStatement();
         } catch (Exception e) {
         System.out.println(e);
